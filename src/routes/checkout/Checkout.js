04933de62bff26384809c70212f4fc48f3df2afa
yih365 <68409635+gigi365@users.noreply.github.com>
2022-04-20 15:29:24 -0700

    checkout

--- src/routes/checkout/Checkout.js
+++ src/routes/checkout/Checkout.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import './Checkout.css'
 
 export default function Checkout() {
 
+  // Example of current order in progress
+  const currentOrderInProgress = true;
+  var currentOrder;
+  if (currentOrderInProgress) {
+    currentOrder = {
+      'id': '934rjfe',
+      'store': 'store1',
+      'items': ['chips', 'chips'],
+      'timeToProcess': 4
+    }
+  }
 
+  const viewCurrentOrderDetails = () => {
+
+  }
 
   return (
-    <h1 className='title'>Checkout</h1>
+    <div>
+      <div>
+        {currentOrderInProgress && 
+          <div id='currentOrder'>
+            <h1>Current Order</h1>
+            <h2>{currentOrder['store']}</h2>
+            <h2>{currentOrder['timeToProcess']}</h2>
+            <button onClick={viewCurrentOrderDetails}>View Details</button>
+          </div>
+        }
+      </div>
+      
+      <h1 className='title'>Past Orders</h1>
+    </div>
   )
 }
